11728e10f4ad0b5072c739bac562c3ac7bd2112a
Sultan Alsawaf <sultan@kerneltoast.com>
2023-04-30 16:23:18 -0700

    Revert "ANDROID: sched: Introducing PELT multiplier"
    This reverts commit 9136e3208e04ca70eee44ebb3c94dc471b231fe3.
    
    Changing the PELT multiplier on the fly disrupts energy and performance
    tuning done using a specific PELT multiplier. Remove it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/pelt.h
+++ kernel/sched/pelt.h
@@ -80,8 +80,6 @@ static inline void _update_idle_rq_clock_pelt(struct rq *rq)
 	u64_u32_store(rq->clock_pelt_idle, rq_clock_pelt(rq));
 }
 
-extern unsigned int sched_pelt_lshift;
-
 /*
  * The clock_pelt scales the time to reflect the effective amount of
  * computation done during the running delta time but then sync back to
@@ -96,10 +94,6 @@ extern unsigned int sched_pelt_lshift;
  */
 static inline void update_rq_clock_pelt(struct rq *rq, s64 delta)
 {
-	delta <<= READ_ONCE(sched_pelt_lshift);
-
-	per_cpu(clock_task_mult, rq->cpu) += delta;
-
 	if (unlikely(is_idle_task(rq->curr))) {
 		_update_idle_rq_clock_pelt(rq);
 		return;
@@ -153,8 +147,7 @@ static inline void update_idle_rq_clock_pelt(struct rq *rq)
 	 * rq's clock_task.
 	 */
 	if (util_sum >= divider)
-		rq->lost_idle_time += rq_clock_task_mult(rq) -
-				      rq->clock_pelt;
+		rq->lost_idle_time += rq_clock_task(rq) - rq->clock_pelt;
 
 	_update_idle_rq_clock_pelt(rq);
 }
