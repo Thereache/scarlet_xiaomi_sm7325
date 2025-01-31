90fdc6e5e80d232849d2bf286d88faabbd988cf8
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-01-31 22:49:41 +0600

    Revert "block: make bio_crypt_clone() able to fail"
    This reverts commit c28433cc55854e096444adbb2cc320af7a41c1b2.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- block/blk-crypto.c
+++ block/blk-crypto.c
@@ -104,13 +104,10 @@ void __bio_crypt_free_ctx(struct bio *bio)
 	bio->bi_crypt_context = NULL;
 }
 
-int __bio_crypt_clone(struct bio *dst, struct bio *src, gfp_t gfp_mask)
+void __bio_crypt_clone(struct bio *dst, struct bio *src, gfp_t gfp_mask)
 {
 	dst->bi_crypt_context = mempool_alloc(bio_crypt_ctx_pool, gfp_mask);
-	if (!dst->bi_crypt_context)
-		return -ENOMEM;
 	*dst->bi_crypt_context = *src->bi_crypt_context;
-	return 0;
 }
 EXPORT_SYMBOL_GPL(__bio_crypt_clone);
 
--- include/linux/blk-crypto.h
+++ include/linux/blk-crypto.h
@@ -121,26 +121,13 @@ static inline bool bio_has_crypt_ctx(struct bio *bio)
 
 static inline void bio_clone_skip_dm_default_key(struct bio *dst,
 						 const struct bio *src);
-
-int __bio_crypt_clone(struct bio *dst, struct bio *src, gfp_t gfp_mask);
-/**
- * bio_crypt_clone - clone bio encryption context
- * @dst: destination bio
- * @src: source bio
- * @gfp_mask: memory allocation flags
- *
- * If @src has an encryption context, clone it to @dst.
- *
- * Return: 0 on success, -ENOMEM if out of memory.  -ENOMEM is only possible if
- *	   @gfp_mask doesn't include %__GFP_DIRECT_RECLAIM.
- */
-static inline int bio_crypt_clone(struct bio *dst, struct bio *src,
-				  gfp_t gfp_mask)
+void __bio_crypt_clone(struct bio *dst, struct bio *src, gfp_t gfp_mask);
+static inline void bio_crypt_clone(struct bio *dst, struct bio *src,
+				   gfp_t gfp_mask)
 {
 	bio_clone_skip_dm_default_key(dst, src);
 	if (bio_has_crypt_ctx(src))
-		return __bio_crypt_clone(dst, src, gfp_mask);
-	return 0;
+		__bio_crypt_clone(dst, src, gfp_mask);
 }
 
 #if IS_ENABLED(CONFIG_DM_DEFAULT_KEY)
